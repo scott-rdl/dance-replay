20a9646bdf5a8f4cb5b6e6fb65e85b7986fe965c
Scott RIDEL <scott.ridel.inc@gmail.com>
2021-04-11 21:52:04 +0200

    add show and cover submit

--- admin.php
+++ admin.php
@@ -9,7 +9,7 @@ include "include/assets-admin.php";
 <section>
     <h1>EDIT</h1><br />
 
-    <h2>ADD Schools</h2>
+    <h2>ADD SCHOOL</h2>
     <form class="admin" method="post" action="">
         <input type="text" name="school-name" placeholder="SCHOOL NAME" required />
         <input type="submit" value="ADD">
@@ -18,9 +18,9 @@ include "include/assets-admin.php";
   
     <br />
     <h2>ADD SHOW</h2>
-    <form class="admin" method="post" action="">
+    <form class="admin" method="post" action="" enctype="multipart/form-data">
 
-        <select name="school" required>
+        <select name="show-school" required>
             <option value="">-- SCHOOL --</option>
 
             <?php 
@@ -33,11 +33,12 @@ include "include/assets-admin.php";
             
         </select><br />
 
-        <input type="text" name="show-name" placeholder="TITLE" required /><br />
-        <input type="number" name="length" placeholder="120" size="5" required /> min /
-        <input type="number" name="size" placeholder="15" size="5" step="0.1" required /> Go<br />
-        <input type="url" name="url" placeholder="https://drive.google.com/uc?export=download&id=..." required><br />
-        <input type="hidden" name="edit" value="add">
+        <input type="text" name="show-title" placeholder="TITLE" required /><br />
+        <input type="date" name="show-date" max="<?php echo date('Y-m-d'); ?>" required /><br />
+        <input type="number" name="show-length" placeholder="120" size="5" required /> min /
+        <input type="number" name="show-size" placeholder="15" size="5" step="0.1" required /> Go<br />
+        <input type="url" name="show-url" placeholder="https://drive.google.com/uc?export=download&id=..." required><br />
+        <input type="file" name="cover" /><br />
         <input type="submit" value="ADD">
 
     </form>
--- include/assets-admin.php
+++ include/assets-admin.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
 
-if (isset($_GET['pass']) ) {
+if (isset($_GET['pass'])) {
     if (password_verify($_GET['pass'], '$2y$10$VtSVwtFt/OrEypQA.daQt.v0u2jv1aGEM0dQDTSrcKy.vIE3chlBm')) {
         $_SESSION['cnx'] = true;
-    } else {unset($_SESSION['cnx']);}
+    } else {
+        unset($_SESSION['cnx']);
+    }
 }
 
 
@@ -11,14 +13,34 @@ if (!isset($_SESSION['cnx'])) {
     header("Location: index.php");
     exit();
 } else {
-    
-    if (isset($_POST['show-name'])) {
-        echo 'new show'.$_POST['show-name'];
+
+    if (isset($_POST['show-title'])) {
         
+        $coverName = coverRename($_FILES['cover']['name']);
+
+        $req = $bdd->prepare("INSERT INTO shows(show_title, show_school, show_length, show_date, show_size, show_url, show_cover ) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $req->execute(array(
+            $_POST['show-title'],
+            $_POST['show-school'],
+            $_POST['show-length'],
+            $_POST['show-date'],
+            $_POST['show-size'],
+            $_POST['show-url'],
+            $coverName
+        ));
+
+        move_uploaded_file($_FILES['cover']['tmp_name'], "img/" . $coverName);
 
     } else if (isset($_POST['school-name'])) {
         $req = $bdd->prepare("INSERT INTO schools(school_name) VALUES (?)");
         $req->execute(array($_POST['school-name']));
     }
+}
 
+
+// Create a new unique name for the image storage
+function coverRename($img)
+{
+    $ext = (new SplFileInfo($img))->getExtension();
+    return time() . '.' . $ext;
 }
